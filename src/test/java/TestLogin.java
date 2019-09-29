e2c8517eb09bdfa4fe24868d398e5f3dfe4f916a
RomaChel <romanchelombitko@gmail.com>
2019-09-29 15:56:19 +0300

    Added a passive test for login in Jira

--- src/test/java/TestLogin.java
+++ src/test/java/TestLogin.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -22,14 +23,15 @@ public class TestLogin {
     By loginButton = By.xpath("//input[@name='login']");
     By wrongPasswordLabel = By.xpath("//div[@class='aui-message aui-message-error']");
     String userName = "Roman_Chelombitko";
-    String password = "AbraKadabra";
+    String passwordWrong = "AbraKadabra";
+    String passwordSuccess = "Roman_Chelombitko";
 
 
     @Test
     public void testLoginWrongLassword() throws InterruptedException {
         this.driver.get("https://jira.hillel.it/login.jsp");
         this.driver.findElement(userNameInput).sendKeys(userName);
-        this.driver.findElement(passwordInput).sendKeys(password);
+        this.driver.findElement(passwordInput).sendKeys(passwordWrong);
         this.driver.findElement(loginButton).click();
 
         Thread.sleep(2000);
@@ -41,4 +43,15 @@ public class TestLogin {
         this.driver.quit();
     }
 
+
+    @Test
+    public void testLoginSuccess() throws InterruptedException {
+        this.driver.get("https://jira.hillel.it/login.jsp");
+        this.driver.findElement(userNameInput).sendKeys(userName);
+        this.driver.findElement(passwordInput).sendKeys(passwordSuccess);
+        this.driver.findElement(loginButton).click();
+
+        Thread.sleep(2000);
+        Assert.assertEquals(driver.getCurrentUrl(),"https://jira.hillel.it/secure/Dashboard.jspa");
+    }
 }
